22e1f4a22b18e93b40c3779389726c05813a85e0
iankolaja <39423229+iankolaja@users.noreply.github.com>
2020-08-10 16:40:41 -0700

    Change to get chat method from slack

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ slack.on('message', function(message) {
       };
 
       // channel.send(response);
-      chat.postEphemeral(token,channel.ID,[{"pretext": "pre-hello", "text": "text-world"}],response,userName)
+      slack.chat.postEphemeral(token,channel.ID,[{"pretext": "pre-hello", "text": "text-world"}],response,userName)
       return console.log("@" + slack.self.name + " responded with \"" + response + "\"");
     }
   } else {
