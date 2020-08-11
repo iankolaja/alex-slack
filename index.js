4c8d7de1fd9ffd37e1693b65e2381ead9492484a
iankolaja <39423229+iankolaja@users.noreply.github.com>
2020-08-10 18:47:45 -0700

    Added missing http

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { WebClient } = require('@slack/web-api');
 const { RTMClient } = require('@slack/rtm-api');
 Alex = require('alex');
+var http = require('http');
  
 // Read a token from the environment variables
 const token = process.env.ALEX_TOKEN;
