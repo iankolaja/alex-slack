06b1f697b391d68369e751cce271ea87f122014e
iankolaja <39423229+iankolaja@users.noreply.github.com>
2020-08-10 18:49:36 -0700

    added connect code

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ rtm.on('message', (event) => {
 	
 });
 
+(async () => {
+  // Connect to Slack
+  const { self, team } = await rtm.start();
+})();
+
 // Heroku requires the process to bind to this port within 60 seconds or it is killed
 http.createServer(function(req, res) {
   res.end('ALEX_SLACK_BOT');
