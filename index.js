6e6d1e0d55b025d1807723520866d4990932a2a5
iankolaja <39423229+iankolaja@users.noreply.github.com>
2020-08-10 18:35:13 -0700

    Updated to use a chaotic combiation of web api and rtm

--- index.js
+++ index.js
@@ -1,102 +1,34 @@
-var Slack, autoMark, autoReconnect, slack, token;
-
-Slack = require('slack-client');
+const { WebClient } = require('@slack/web-api');
+const { RTMClient } = require('@slack/rtm-api');
 Alex = require('alex');
-var http = require('http');
+ 
+// Read a token from the environment variables
+const token = process.env.ALEX_TOKEN;
+ 
+// Initialize
+const web = new WebClient(token);
+const rtm = new RTMClient(token);
 
-token = process.env.ALEX_TOKEN;
+rtm.on('message', (event) => {
+  var channel, user, text;
+  channel = event.channel;
+  user = event.user;
+  text = event.text;
+  var response = ""
+  var a_messages = Alex(text).messages;
 
-autoReconnect = true;
-
-autoMark = true;
-
-slack = new Slack(token, autoReconnect, autoMark);
-
-slack.on('open', function() {
-  var channel, channels, group, groups, id, messages, unreads;
-  channels = [];
-  groups = [];
-  unreads = slack.getUnreadCount();
-  channels = (function() {
-    var ref, results;
-    ref = slack.channels;
-    results = [];
-    for (id in ref) {
-      channel = ref[id];
-      if (channel.is_member) {
-        results.push("#" + channel.name);
-      }
+  if(a_messages.length) {
+    if (!channel.is_im) {
+	response += "You said: \"" + text + '\n';
     }
-    return results;
-  })();
-  groups = (function() {
-    var ref, results;
-    ref = slack.groups;
-    results = [];
-    for (id in ref) {
-      group = ref[id];
-      if (group.is_open && !group.is_archived) {
-        results.push(group.name);
-      }
-    }
-    return results;
-  })();
-  console.log("Welcome to Slack. You are @" + slack.self.name + " of " + slack.team.name);
-  console.log('You are in: ' + channels.join(', '));
-  console.log('As well as: ' + groups.join(', '));
-  messages = unreads === 1 ? 'message' : 'messages';
-  return console.log("You have " + unreads + " unread " + messages);
+
+  for (var i = 0; i < a_messages.length; i++) {
+  response += Alex(text).messages[i].reason + '\n';
+  };
+  chat.postEphemeral(token,[],channel,response,user)
+  return console.log("@" + slack.self.name + " responded with \"" + response + "\"");
+	
 });
-
-slack.on('message', function(message) {
-  var channel, channelError, channelName, errors, response, text, textError, ts, type, typeError, user, userName;
-  channel = slack.getChannelGroupOrDMByID(message.channel);
-  user = slack.getUserByID(message.user);
-  response = '';
-  type = message.type, ts = message.ts, text = message.text;
-  channelName = (channel != null ? channel.is_channel : void 0) ? '#' : '';
-  channelName = channelName + (channel ? channel.name : 'UNKNOWN_CHANNEL');
-  userName = (user != null ? user.name : void 0) != null ? "@" + user.name : "UNKNOWN_USER";
-  console.log("Received: " + type + " " + channelName + " " + userName + " " + ts + " \"" + text + "\"");
-  if (type === 'message' && (text != null) && (channel != null)) {
-
-    var a_messages = Alex(text).messages;
-
-    if(a_messages.length) {
-	  if (!channel.is_im) {
-	  	response += "You said: \"" + text + "\" in " + channelName + '\n';
-	  }
-	  
-      for (var i = 0; i < a_messages.length; i++) {
-        response += Alex(text).messages[i].reason + '\n';
-      };
-
-	  if (channel.is_im) {
-		channel.send(response);
-	  } else {
-	  	slack.openDM(message.user, function(data){
-	  	  dm = slack.getChannelGroupOrDMByID(data.channel.id);
-		  dm.send(response);
-	  	});
-	  }
-      return console.log("@" + slack.self.name + " responded with \"" + response + "\"");
-    }
-  } else {
-    typeError = type !== 'message' ? "unexpected type " + type + "." : null;
-    textError = text == null ? 'text was undefined.' : null;
-    channelError = channel == null ? 'channel was undefined.' : null;
-    errors = [typeError, textError, channelError].filter(function(element) {
-      return element !== null;
-    }).join(' ');
-    return console.log("@" + slack.self.name + " could not respond. " + errors);
-  }
-});
-
-slack.on('error', function(error) {
-  return console.error("Error: " + error);
-});
-
-slack.login();
 
 // Heroku requires the process to bind to this port within 60 seconds or it is killed
 http.createServer(function(req, res) {
